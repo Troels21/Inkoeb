d5893e136926809e7c79bb945dc5d22d391d48bb
Troels21 <Troels.e.kiib@gmail.com>
2022-11-15 10:23:12 +0100

    Log4J

--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -1,16 +1,19 @@
 package launch;
 
+import lombok.extern.log4j.Log4j2;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
 import java.io.File;
+@Log4j2
 
 public class Main {
     public static void main(String[] args) {
         Tomcat tomcat = new Tomcat();
         tomcat.setBaseDir("temp");
         String port = "8080";
-
+        log.error("Error-logging is working!");
+        log.info("Launching Application");
 
         tomcat.setPort(Integer.parseInt(port));
         tomcat.getConnector();
