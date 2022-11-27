346a1ecf90f59bb958d307c1a26a6ab516a24250
Troels21 <Troels.e.kiib@gmail.com>
2022-11-27 14:49:50 +0100

    Log4J

--- reactappen/src/App.js
+++ reactappen/src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import Indhold from "./Component/Indhold";
 import {indkoebStore} from "./Stores/IndkoebStore";
+import {BrowserTracing} from "@sentry/tracing";
+import * as Sentry from "@sentry/react";
 
+Sentry.init({
+    dsn: "https://71379769231243c4a6ecb1b4484fd645@o4504162458664960.ingest.sentry.io/4504162460303360",
+    integrations: [new BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+});
 
 function App() {
     indkoebStore.fetchVarer()
     return (
-        <Indhold/>
+    <Indhold/>
     );
 }
 
--- src/main/java/launch/Main.java
+++ src/main/java/launch/Main.java
@@ -3,17 +3,33 @@ package launch;
 import lombok.extern.log4j.Log4j2;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
+import io.sentry.Sentry;
 
 import java.io.File;
 @Log4j2
 
 public class Main {
     public static void main(String[] args) {
+
+        Sentry.init(options -> {
+            options.setDsn("https://71379769231243c4a6ecb1b4484fd645@o4504162458664960.ingest.sentry.io/4504162460303360");
+            // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+            // We recommend adjusting this value in production.
+            options.setTracesSampleRate(1.0);
+            // When first trying Sentry it's good to see what the SDK is doing:
+            options.setDebug(true);
+        });
+
         Tomcat tomcat = new Tomcat();
         tomcat.setBaseDir("temp");
         String port = "8080";
         log.error("Error-logging is working!");
         log.info("Launching Application");
+        try {
+            throw new Exception("This is a test.");
+        } catch (Exception e) {
+            Sentry.captureException(e);
+        }
 
         tomcat.setPort(Integer.parseInt(port));
         tomcat.getConnector();
